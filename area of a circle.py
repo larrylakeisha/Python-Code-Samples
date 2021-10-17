8975ce628e1ff1a9928fc288a92987b6e6f13dc9
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-16 22:40:34 -0500

    Update area of a circle.py
    I added the comments for explaining radius and area.
    And you need to use input statement if user enters a radius.

--- area of a circle.py
+++ area of a circle.py
@@ -4,11 +4,12 @@
 #This program computes the area of a circle
 
 
-print("Please enter the radius")
+print("Please enter the radius")  #input variable: radius is supposed to be given by user as the print statement says.
 
-radius = 20
+radius = 20                       #input statement is required instead assignment.
+                                  
 
-pi = 3.14
-area = pi*(radius**2)
+pi = 3.14                       
+area = pi*(radius**2)             #output variable, area is computed for the circle  
 
 print("Thank you, here is your answer", area)
